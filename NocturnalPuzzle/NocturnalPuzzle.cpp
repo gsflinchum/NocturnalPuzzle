2296862e0e20208f1da8d33ccdb84f98c6a668dd
Graham Flinchum <graham.flinchum@gmail.com>
2022-08-01 22:18:07 -0400

    Cleaning up, adding comments, commenting out some debugging code

--- NocturnalPuzzle/NocturnalPuzzle.cpp
+++ NocturnalPuzzle/NocturnalPuzzle.cpp
@@ -3,8 +3,6 @@
 
 #include "NocturnalPuzzle.h"
 
-using namespace std;
-
 void find_word(const char word[], const int size, char puzzle[][20], int coords[])
 {
 	bool match = false;
@@ -71,6 +69,8 @@ void read_puzzles(const char* filename, char arr1[][20], char arr2[][20])
 	char c;
 	int i = 0;
 	int j = 0;
+	// don't start pulling in letters until after the brackets start
+	// assume the puzzle size is 20x20
 	do { c = fgetc(fp); } while (c != '[');
 	while (c != EOF) {
 		if ((97 <= c) && (c <= 122)) {
@@ -86,6 +86,7 @@ void read_puzzles(const char* filename, char arr1[][20], char arr2[][20])
 	}
 	i = 0;
 	j = 0;
+	// same as logic for first puzzle
 	do { c = fgetc(fp); } while (c != '[');
 	while (c != EOF) {
 		if ((97 <= c) && (c <= 122)) {
@@ -98,14 +99,13 @@ void read_puzzles(const char* filename, char arr1[][20], char arr2[][20])
 			}
 		}
 		c = fgetc(fp);
-
 	}
 	fclose(fp);
-	
 }
 
 int main()
 {
+	const char* filename = "C:\\Users\\graha\\source\\repos\\NocturnalPuzzle\\NocturnalPuzzle\\puzzle.txt";
 	char puzzle1[20][20] = { {} };
 	char puzzle2[20][20] = { {} };
 	char word1[] = "nocturnal";
@@ -113,7 +113,8 @@ int main()
 	int coords1[2] = {21,21};
 	int coords2[2] = {21,21};
 	
-	read_puzzles("C:\\Users\\graha\\source\\repos\\NocturnalPuzzle\\NocturnalPuzzle\\puzzle.txt", puzzle1, puzzle2);
+	read_puzzles(filename, puzzle1, puzzle2);
+	// try to find the word "nocturnal" up to 25 times, rotating each time it isn't found
 	for (int rot1 = 0; rot1 < 26; rot1++) {
 		find_word(word1, 9, puzzle1, coords1);
 		if (coords1[0] == 21) {
@@ -124,7 +125,7 @@ int main()
 			break;
 		}
 	}
-
+	// try to find the word "defibrillator" up to 25 times, rotating each time it isn't found
 	for (int rot2 = 0; rot2 < 26; rot2++) {
 		find_word(word2, 13, puzzle2, coords2);
 		if (coords2[0] == 21){
@@ -136,21 +137,21 @@ int main()
 		}
 	}
 
-
-
-	// Print out arrays for testing
+	// Print out arrays for visual testing
+	/*
 	for (int i = 0; i < 20; i++) {
 		for (int j = 0; j < 20; j++) {
-			cout << puzzle1[i][j];
+			printf("%c", puzzle1[i][j]);
 		}
-		cout << endl;
+		printf("\n");
 	}
-	cout << endl;
+	printf("\n");
 	for (int i = 0; i < 20; i++) {
 		for (int j = 0; j < 20; j++) {
-			cout << puzzle2[i][j];
+			printf("%c", puzzle2[i][j]);
 		}
-		cout << endl;
+		printf("\n");
 	}
+	*/
 	return 0;
 }
