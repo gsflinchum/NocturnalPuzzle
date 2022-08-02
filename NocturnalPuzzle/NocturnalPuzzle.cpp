31eab43aae8b137e0f985d27948adeb550134877
Graham Flinchum <graham.flinchum@gmail.com>
2022-08-01 22:07:15 -0400

    Adding logic for finding words in the 20x20 puzzles.

--- NocturnalPuzzle/NocturnalPuzzle.cpp
+++ NocturnalPuzzle/NocturnalPuzzle.cpp
@@ -5,16 +5,49 @@
 
 using namespace std;
 
-bool find_word(const char word[], char puzzle[][20])
+void find_word(const char word[], const int size, char puzzle[][20], int coords[])
 {
+	bool match = false;
 	for (int i = 0; i < 20; i++) {
 		for (int j = 0; j < 20; j++) {
 			if (word[0] == puzzle[i][j]) {
-
+				// check if horizontal length is possible
+				if ((20 - j) >= size) {
+					// check for horizontal match
+					for (int k = 1; k < size; k++) {
+						if (word[k] != puzzle[i][j + k]) {
+							// letter isn't a match, break and check vertically
+							break;
+						}
+						else if (k == size - 1) {
+							match = true;
+							coords[0] = i;
+							coords[1] = j;
+							return;
+						}
+					}
+				}
+				// check if vertical length is possible
+				if ((20 - i) >= size) {
+					// check for vertical match
+					for (int k = 1; k < size; k++) {
+						if (word[k] != puzzle[i + k][j]) {
+							// letter isn't a match, break and continue searching
+							break;
+						}
+						else if (k == size - 1) {
+							match = true;
+							coords[0] = i;
+							coords[1] = j;
+							return;
+						}
+					}
+				}
+				
 			}
 		}
 	}
-	return 0;
+	return;
 }
 
 void rotate_puzzle(char puzzle[][20])
@@ -73,12 +106,38 @@ void read_puzzles(const char* filename, char arr1[][20], char arr2[][20])
 
 int main()
 {
-	char puzzle1[20][20] = {{}};
-	char puzzle2[20][20] = {{}};
-	cout << "Hello CMake." << endl;
+	char puzzle1[20][20] = { {} };
+	char puzzle2[20][20] = { {} };
+	char word1[] = "nocturnal";
+	char word2[] = "defibrillator";
+	int coords1[2] = {21,21};
+	int coords2[2] = {21,21};
 	
 	read_puzzles("C:\\Users\\graha\\source\\repos\\NocturnalPuzzle\\NocturnalPuzzle\\puzzle.txt", puzzle1, puzzle2);
-	rotate_puzzle(puzzle1);
+	for (int rot1 = 0; rot1 < 26; rot1++) {
+		find_word(word1, 9, puzzle1, coords1);
+		if (coords1[0] == 21) {
+			rotate_puzzle(puzzle1);
+		}
+		else {
+			printf("x=%d, y=%d, count=%d\n", coords1[0], coords1[1], rot1);
+			break;
+		}
+	}
+
+	for (int rot2 = 0; rot2 < 26; rot2++) {
+		find_word(word2, 13, puzzle2, coords2);
+		if (coords2[0] == 21){
+			rotate_puzzle(puzzle2);
+		}
+		else {
+			printf("x=%d, y=%d, count=%d\n", coords2[0], coords2[1], rot2);
+			break;
+		}
+	}
+
+
+
 	// Print out arrays for testing
 	for (int i = 0; i < 20; i++) {
 		for (int j = 0; j < 20; j++) {
@@ -86,6 +145,7 @@ int main()
 		}
 		cout << endl;
 	}
+	cout << endl;
 	for (int i = 0; i < 20; i++) {
 		for (int j = 0; j < 20; j++) {
 			cout << puzzle2[i][j];
